86cf5085b66f81ff6c161c463b175882b2999f0c
kbabawale <kolapobabawale@gmail.com>
2024-09-01 18:33:48 +0100

    prod base url

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     env: mode === 'development' ? {
         API_HOST: 'http://localhost:3000/api/v1'
     } : {
-        API_HOST: 'http://localhost:3000/api/v1'
+        API_HOST: 'https://payout-gamma.vercel.app/api/v1'
     },
     reactStrictMode: mode === 'development' ? true : false,
 };
